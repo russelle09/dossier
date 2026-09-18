bef527d7854f508ae18300cc1af5d875ef8d138e
MKRUSSELLE-e <ngamaleumonkamrussellevire@gmail.com>
2026-09-18 12:36:19 +0100

    code qui calcule deux nombres entres par l'utilisateur

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main()
+{
+    int a , x ;
+    std::cout<<"donner un chiffre";
+    std::cin>>a;
+    std::cout<<"donner un autre chiffre";
+    std::cin>>x;
+    std::cout<<a+x;
+  
+    return 0;
+}
