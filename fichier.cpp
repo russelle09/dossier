6e81c71ed7bcfc8af12be4c9efd5b01448d73048
MKRUSSELLE-e <ngamaleumonkamrussellevire@gmail.com>
2026-09-18 11:42:18 +0100

    code qui affiche bonjour tant que le nombre entrer est inferieur a 5

---
+++ fichier.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main()
+{
+    int a = 5, x = 0;
+    do{
+
+   std::cout<<"votre chiffre"; 
+   std::cin>>x;
+  std::cout<<"bonjour a tous";  }
+  while(x<a);
+
+    return 0;
+}
